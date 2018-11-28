aa96480d794e3fb0d17361cc2ed26b1fe5feab2a
abrahamrhoffman <abrahamrhoffman@gmail.com>
2018-11-28 16:12:42 -0700

    0.9.9.2

--- anaxdb/__init__.py
+++ anaxdb/__init__.py
@@ -1,1 +1,3 @@
-name = "anaxdb"
+__version__ = "0.9.9.2"
+
+from anaxdb import Database
--- setup.py
+++ setup.py
@@ -5,13 +5,23 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="anaxdb",
-    version="0.9.3",
+    version="0.9.9.2",
     author="Abraham Hoffman",
     author_email="abrahamrhoffman@gmail.com",
     description="An encrypted non-linear database based on Pandas",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/abrahamrhoffman/anaxdb",
+    install_requires=[
+        'pandas==0.23.4',
+        'numpy==1.15.4',
+        'pyarrow==0.11.1',
+        'matplotlib==2.2.3',
+        'seaborn==0.9.0',
+        'pycryptodome==3.7.2',
+        'pathlib==1.0.1',
+        'minio==4.0.6'
+    ],
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 2",
