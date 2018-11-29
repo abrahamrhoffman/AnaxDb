ae1cd01ce43c21783ca11e6c7cf93445c9d0de55
abrahamrhoffman <abrahamrhoffman@gmail.com>
2018-11-29 08:33:07 -0700

    1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="anax",
-    version="0.9",
+    version="1.0",
     author="Abraham Hoffman",
     author_email="abrahamrhoffman@gmail.com",
     description="An encrypted non-linear database based on Pandas",
